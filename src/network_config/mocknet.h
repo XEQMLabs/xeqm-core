c47e017efa77af81fdf4631518de13fc16e3e7cf
doylet <doyle.github.2ifxi@simplelogin.com>
2025-02-24 11:09:27 +1100

    Mocknet changes to work on mainnet
    - Address some issues with the network not correctly transitioning to
    mocknet when a height is given that has already passed.
    - Fix block leader not being set in the cache in certain scenarios
    causing bad blocks to be generated
    - Ensure that the Pulse quorum is hijacked at the leaf-most function for
    generating a quorum to ensure all code paths requesting a Pulse quorum correctly
    get hijacked with mock nodes
    - Simplify the mocknet interface by dropping the post CN block add hook
    and instead kill P2P comms when a mock pulse block is being constructed

--- src/network_config/mocknet.h
+++ src/network_config/mocknet.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <crypto/crypto.h>
+
 #if defined(WITH_MOCKNET)
 #include <stdint.h>
 namespace service_nodes {
@@ -20,18 +22,18 @@ bool mocknet_read_cli_for_mocknet_arg(
         const boost::program_options::variables_map& vm, bool is_service_node);
 bool mocknet_is_forking(uint64_t top_block_height);
 bool mocknet_has_forked(uint64_t top_block_height);
+crypto::public_key mocknet_get_deterministic_block_leader();
 void mocknet_replace_quorum_with_mock_nodes(
         service_nodes::quorum& quorum, uint64_t top_block_height);
 void mocknet_inject_nodes(uint8_t nettype, void* snl_state_ptr, uint8_t hf_version);
-void mocknet_on_cn_core_post_add_new_block(cryptonote::core& core);
 void mocknet_push_mock_pulse_block(cryptonote::core& core);
 #else
 #define mocknet_add_cli_arg(...)
 #define mocknet_read_cli_for_mocknet_arg(...) true
 #define mocknet_is_forking(...) false
 #define mocknet_has_forked(...) false
+#define mocknet_get_deterministic_block_leader(...) crypto::null<crypto::public_key>
 #define mocknet_replace_quorum_with_mock_nodes(...)
-#define mocknet_on_snl_block_add(...)
-#define mocknet_on_cn_core_post_add_new_block(...)
+#define mocknet_inject_nodes(...)
 #define mocknet_push_mock_pulse_block(...)
 #endif
