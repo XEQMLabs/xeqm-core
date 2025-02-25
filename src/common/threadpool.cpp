d2d87f7840021272913bb2a7d34809ff5409df54
Jason Rhinelander <jason@imaginary.ca>
2025-02-25 19:44:07 -0400

    Check condition with the lambda version of wait_for

--- src/common/threadpool.cpp
+++ src/common/threadpool.cpp
@@ -133,9 +133,8 @@ void threadpool::waiter::wait(threadpool* tpool) {
 }
 
 bool threadpool::waiter::wait_for(const std::chrono::milliseconds& t) {
-    if (num == 0) return true;
     std::unique_lock lock{mt};
-    return cv.wait_for(lock, t) == std::cv_status::no_timeout && num == 0;
+    return cv.wait_for(lock, t, [this] { return num == 0; });
 }
 
 void threadpool::waiter::inc() {
