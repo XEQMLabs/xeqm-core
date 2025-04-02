c3bd74ceb126dd26b0035df645f649920a253151
Jason Rhinelander <jason@imaginary.ca>
2025-04-02 18:39:02 -0300

    Add snode address change OMQ subscription
    Currently there are bugs in storage server around noticing new IP
    addresses.  This adds a mechanism into oxend so that storage server can
    subscribe to snode IP/port address changes to get them pushed as soon as
    oxend received the uptime proof with the new address, so that storage
    server will be able to update the record it uses to reach other storage
    servers as soon as possible.

--- src/cryptonote_core/uptime_proof.h
+++ src/cryptonote_core/uptime_proof.h
@@ -45,6 +45,15 @@ class Proof {
     uint16_t storage_omq_port{};
     uint16_t qnet_port{};
 
+    // Returns true if this Proof and the given Proof value have different contact-dependent fields:
+    // that is, if any of the ed pubkey, IP, or ports of various services are different across the
+    // two proofs.
+    bool contact_info_changed(const Proof& other) const {
+        return pubkey_ed25519 != other.pubkey_ed25519 || public_ip != other.public_ip ||
+               storage_https_port != other.storage_https_port ||
+               storage_omq_port != other.storage_omq_port || qnet_port != other.qnet_port;
+    }
+
     // Non-consensus field: stores the current git version or release tag to assist debugging.
     std::string version_tag;
 
